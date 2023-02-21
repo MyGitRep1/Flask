574456a5afeed5288625ae8234fa2ec5e9ac4ff1
MyGitRep1 <uzinovev26@gmail.com>
2023-02-21 20:13:16 +0300

    Created view with 'x and y', imported BadRequest from Werkzeug

--- blog/app.py
+++ blog/app.py
@@ -1,20 +1,30 @@
-from datetime import time
-from flask import Flask, g
-
+from flask import Flask, request
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
-@app.before_request
-def process_before_request():
-    """
-    Sets start_time to `g` object
-    """
-    g.start_time = time()
-@app.after_request
-def process_after_request(response):
-    """
-    adds process time in headers
-    """
-    if hasattr(g, "start_time"):
-        response.headers["process-time"] = time() - g.start_time
-    return response
+@app.route("/power/")
+def power_value():
+    x = request.args.get("x") or ""
+    y = request.args.get("y") or ""
+    if not (x.isdigit() and y.isdigit()):
+        app.logger.info("invalid values for power: x=%r and y=%r", x, y)
+        raise BadRequest("please pass integers in `x` and `y` query params")
+    x = int(x)
+    y = int(y)
+    result = x ** y
+    app.logger.debug("%s ** %s = %s", x, y, result)
+    return str(result)
+
+
+# GET (no values passed) http://127.0.0.1:5000/power/
+# < HTTP/1.0 400 BAD REQUEST
+# - Bad Request
+# please pass integers in `x` and `y` query params
+# [log]: INFO in app: invalid values for power: x='' and y=''
+
+
+# GET (pass valid data) http://127.0.0.1:5000/power/?x=7&y=3
+# < HTTP/1.0 200 OK
+# - 343
+# [log]: DEBUG in app: 7 ** 3 = 343
