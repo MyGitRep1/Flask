4b844e0ad5b49c5cd946b618a051cc7e9e2cc5c1
MyGitRep1 <uzinovev26@gmail.com>
2023-02-21 19:25:58 +0300

    Created a new view with 'query string'

--- blog/app.py
+++ blog/app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return 'Hello!'
+@app.route("/user/")
+def read_user():
+    name = request.args.get("name")
+    surname = request.args.get("surname")
+    return f"User {name or '[no name]'} {surname or '[no surname]'}"
