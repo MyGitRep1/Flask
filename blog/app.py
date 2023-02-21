20432ebe9c85be29ebc5032d1e5709131cf60744
MyGitRep1 <uzinovev26@gmail.com>
2023-02-21 20:03:33 +0300

    Created object: 'g'

--- blog/app.py
+++ blog/app.py
@@ -1,44 +1,20 @@
-from flask import Flask, request
+from datetime import time
+from flask import Flask, g
+
 
 app = Flask(__name__)
 
-
-@app.route("/status/", methods=["GET", "POST"])
-def custom_status_code():
-    if request.method == "GET":
-        return """\
-To get response with custom status code
-send request using POST method
-and pass `code` in JSON body / FormData
-"""
-    print("raw bytes data:", request.data)
-    if request.form and "code" in request.form:
-        return "code from form", request.form["code"]
-    if request.json and "code" in request.json:
-        return "code from json", request.json["code"]
-    return "", 204
-
-
-# GET
-# > curl --request GET --url http://127.0.0.1:5000/status/
-# - To get response with custom status code
-#  send request using POST method
-#  and pass `code` in JSON body / FormData
-
-# POST (empty body)
-# > curl --request POST --url http://127.0.0.1:5000/status/
-# < HTTP/1.0 204 NO CONTENT
-
-# POST (multipart/form-data)
-# > curl --request POST --url http://127.0.0.1:5000/status/ \
-# --header 'Content-Type: multipart/form-data' \
-# --form code=202
-# < HTTP/1.0 202
-# - code from form
-
-# POST (json)
-# > curl --request POST --url http://127.0.0.1:5000/status/ \
-# --header 'Content-Type: application/json' \
-# --data '{"code": 205}'
-# < HTTP/1.0 205 RESET CONTENT
-# - code from json
+@app.before_request
+def process_before_request():
+    """
+    Sets start_time to `g` object
+    """
+    g.start_time = time()
+@app.after_request
+def process_after_request(response):
+    """
+    adds process time in headers
+    """
+    if hasattr(g, "start_time"):
+        response.headers["process-time"] = time() - g.start_time
+    return response
