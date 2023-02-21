71b8005be8e62f9d36ce2ec38b4681b6f3e943bc
MyGitRep1 <uzinovev26@gmail.com>
2023-02-21 20:54:23 +0300

    Added

--- blog/app.py
+++ blog/app.py
@@ -16,3 +16,16 @@ def handle_zero_division_error(error):
 
 # < HTTP/1.0 400 BAD REQUEST
 # - Never divide by zero!
+
+# Process unhandled zero division error
+#    [log]: ERROR in app: Here's traceback for zero division error
+#        Traceback (most recent call last):
+#          File "/usr/lib/python3.9/site-packages/flask/app.py", line 1950, in
+# full_dispatch_request
+#            rv = self.dispatch_request()
+#          File "/usr/lib/python3.9/site-packages/flask/app.py", line 1936, in
+# dispatch_request
+#            return self.view_functions[rule.endpoint](**req.view_args)
+#         File "/apps/flask-lesson/blog/app.py", line 155, in do_zero_division
+#           return 1 / 0
+#        ZeroDivisionError: division by zero
